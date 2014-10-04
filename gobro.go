e920fbec46791f4e7abd16bb8e242f9987a94279
Sean Pont <seanpont@gmail.com>
2014-10-04 15:56:58 -0700

    main func command parsing

--- gobro.go
+++ gobro.go
@@ -3,6 +3,7 @@ package gobro
 import (
 	"fmt"
 	"os"
+	"reflect"
 	"runtime"
 	"strings"
 )
@@ -11,12 +12,30 @@ import (
 // Message is optional. Including more than one message will not have any result.
 func ExitOnError(err error, message ...string) {
 	if err != nil {
-		errorMessage := caller() + err.Error()
+		var msg string
 		if len(message) > 0 {
-			errorMessage += message[0]
+			msg = message[0] + " "
 		}
+		errorMessage := caller() + msg + err.Error()
 		fmt.Fprintf(os.Stderr, errorMessage+"\n")
 		os.Exit(1)
+	}
+}
+
+func LogError(err error, message ...string) {
+	if err != nil {
+		var msg string
+		if len(message) > 0 {
+			msg = message[0] + " "
+		}
+		errorMessage := caller() + msg + err.Error()
+		fmt.Fprintf(os.Stderr, errorMessage+"\n")
+	}
+}
+
+func TrimAll(items []string) {
+	for i, item := range items {
+		items[i] = strings.Trim(item, " \n\r\t")
 	}
 }
 
@@ -26,4 +45,47 @@ func caller() string {
 	caller := strings.Split(string(stack[:n]), "\n")[6]
 	caller = strings.Trim(caller, " \t")
 	return strings.Split(caller, " ")[0] + ": "
+}
+
+// ===== COMMAND MAPPER ======================================================
+
+type CommandMap struct {
+	// CommandMap holds a map of names to functions. Useful for handling
+	// control flow in main functions writing a ton of if this else that or
+	// using flag, which I find sub-optimal
+	commandMap map[string]func([]string)
+}
+
+func NewCommandMap(functions ...func(args []string)) (commandMap CommandMap) {
+	// Returns a new CommandMap with the functions mapped to their names.
+	commandMap.commandMap = make(map[string]func([]string))
+
+	for _, fn := range functions {
+		name := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+		name = strings.Split(name, ".")[1] // main.command becomes command
+		commandMap.commandMap[name] = fn
+	}
+
+	return
+}
+
+func (cm CommandMap) Run(args []string) {
+
+	if len(os.Args) == 1 {
+		fmt.Printf("Usage: %s [options] <command> [<args>]\n\n", args[0])
+		fmt.Println("Available commands:")
+		for k, _ := range cm.commandMap {
+			fmt.Printf("   %s\n", k)
+		}
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	fn := cm.commandMap[command]
+	if fn == nil {
+		fmt.Fprintf(os.Stderr, "Command '%s' not found\n", command)
+		os.Exit(1)
+	}
+
+	fn(os.Args[2:])
 }
